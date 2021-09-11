fc4bc2db00ea24e2ec1fe881a457413b3c8d9e02
Lee Jae Hyun <myleje@gmail.com>
2021-09-11 16:23:18 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -14,7 +14,25 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0, 0, 0.1f);
+        float h = Input.GetAxis("Horizontal"); // -1.0f ~ 0.0f ~ +1.0f
+        float v = Input.GetAxis("Vertical");  // -1.0f ~ 0.0f ~ +1.0f
+
+        // 콘솔뷰에 메시지를 출력
+        Debug.Log("h=" + h + ",v=" + v);
+
+        transform.Translate(Vector3.forward * 0.1f);
+
+        /*
+            정규화 벡터(Normalized Vector), 단위 벡터(Unit Vector)
+
+            Vector3.forward = Vector3(0, 0, 1)
+            Vector3.up      = Vector3(0, 1, 0)
+            Vector3.right   = Vector3(1, 0, 0)
+
+            Vector3.one = Vector3(1, 1, 1)
+            Vector3.zero = Vector3(0, 0, 0)
+        */
+
     }
 
 }
