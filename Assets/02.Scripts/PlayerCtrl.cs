33209df6757a9af97183a39b8927aeb1046e9644
Lee Jae Hyun <myleje@gmail.com>
2021-09-11 15:27:24 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(0, 0, 0.1f);
     }
+
 }
