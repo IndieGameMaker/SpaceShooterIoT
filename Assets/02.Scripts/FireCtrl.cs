15f80507abb5b0af4df9150a43335e2b78e23aab
Lee Jae Hyun <myleje@gmail.com>
2021-09-12 11:37:04 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -16,6 +16,11 @@ public class FireCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            // 총알 생성
+            // Instantiate (생성할객체, 위치, 각도)
+            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        }
     }
 }
