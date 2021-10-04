967c75c31fc62b82a3569925c8c3d5e6bfa932ca
Lee Jae Hyun <myleje@gmail.com>
2021-10-04 10:42:59 +0900

    .

--- Assets/02.Scripts/MonsterCtrl.cs
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -5,6 +5,17 @@ using UnityEngine.AI;
 
 public class MonsterCtrl : MonoBehaviour
 {
+    // 몬스터의 상태 정의
+    public enum State
+    {
+        IDLE,
+        TRACE,
+        ATTACK,
+        DIE
+    }
+    // 몬스터의 상태를 저장할 변수
+    public State state = State.IDLE;
+
     public Transform playerTr;
     private NavMeshAgent agent;
 
@@ -21,3 +32,11 @@ public class MonsterCtrl : MonoBehaviour
         agent.SetDestination(playerTr.position);
     }
 }
+
+/*
+    유한상태머신(Finite State Machine) FSM
+    - NPC AI 구현하기 위한 기법(방식)
+
+    - HFSM
+    - BT
+*/
